8c6be5a6c6e8abdd017497142e32b5e27128f97e
makelloss <artemvakhno@gmail.com>
2022-09-28 23:01:40 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$users1 = ["John" => "qwerty", "Nicole" => "asdf", "Mark" => "ww"];
+$users2 = ["Joan" => "1234", "Mark" => "poiu", "Nicole" => "ggg"];
+$result = array_merge($users1, $users2);
+print_r($result);
+?>
